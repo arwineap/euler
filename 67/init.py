26488ed54936eeea7217bf1da3a1bdad7e993550
alex <arwineap@gmail.com>
2014-03-23 18:04:56 -0700

    Moved the problem 18 refactor to problem 67
    Summary:
    Following the success of problem 18, i've moved the new solution to problem 67
    
    Fixes T2
    
    Test Plan: none
    
    Reviewers: alex
    
    Reviewed By: alex
    
    Maniphest Tasks: T2
    
    Differential Revision: http://phab.asshat.net/D2

--- 67/init.py
+++ 67/init.py
@@ -1,46 +1,49 @@
 #!/usr/bin/env python
 
 
-def nextPathPoint(triangle, startpoint):
-    # triangle should be the 2d list of value
-    # startpoint should be the [x][y] coords of triangle to get where we're at
-    # TODO: resolution should be how far down the rabbit hole we should go
-    #
-    # options = Layer one of options under startpoint
-    options = getNextPoints(triangle, startpoint)
-    optiondict = {}
-    for option in options:
-        optiondict[option] = {}
-        optiondict[option]['value'] = getValue(triangle, option)
-        optiondict[option]['potential'] = optiondict[option]['value']
-        optiondict[option]['options'] = getNextPoints(triangle, option)
-        for optx in optiondict[option]['options']:
-            maxvalue = 0
-            for foo in getNextPoints(triangle, optx):
-                if getValue(triangle, foo) > maxvalue:
-                    maxvalue = getValue(triangle, foo)
-        optiondict[option]['potential'] = optiondict[option]['potential'] + maxvalue
-    maxvalue = 0
-    for key in optiondict:
-        if optiondict[key]['potential'] > maxvalue:
-            maxvalue = optiondict[key]['potential']
-            maxkey = key
-    return maxkey
+def makechoice(triangle, coords):
+    # I need to know how many levels of triangle to fold up off the bottom
+    # on small-input, at y index 12 we need to fold the last two rows together
+    # len(triangle) is 15.
+    # at y index 8, I need 5 iterations
+    # at y index 12 I need 1 iteration
+    # this means my 'effective' height is len()-2
+    triheight = len(triangle)-2
+    iterations = triheight-coords[0]
+    # fold the triangle array to make it easier for the dumb choice function
+    for x in range(iterations):
+        triangle = foldtriangle(triangle)
+    return dumbchoice(triangle, coords)
+
+
+def dumbchoice(triangle, coords):
+    option1 = (coords[0]+1, coords[1])
+    option2 = (coords[0]+1, coords[1]+1)
+    if getValue(triangle, option1) > getValue(triangle, option2):
+        return option1
+    else:
+        return option2
+
+
+def foldtriangle(triangle):
+    newrow = []
+    lastrowidx = len(triangle)-1
+    # triangle is zero indexed
+    for i, entry in enumerate(triangle[-2:-1][0]):
+        option1 = entry + triangle[lastrowidx][i]
+        option2 = entry + triangle[lastrowidx][i+1]
+        if option1 > option2:
+            newrow.append(option1)
+        else:
+            newrow.append(option2)
+    result = triangle[:-2]
+    result.append(newrow)
+    return result
 
 
 def getValue(triangle, coords):
     return int(triangle[coords[0]][coords[1]])
 
-
-def getNextPoints(triangle, startpoint):
-    # returns a list of next coordinates:
-    # return [(x,y), (x1, y1)]
-    nextrow = int(startpoint[0])+1
-    nextvalue = int(startpoint[1])+1
-    choices = []
-    choices.append((nextrow, startpoint[1]))
-    choices.append((nextrow, nextvalue))
-    return choices
 
 # load triangle file
 fh = open('input', 'r')
@@ -50,46 +53,20 @@ fh.close()
 # break triangle var up into a 2d array so I can use it
 parsedinput = []
 for line in rawinput:
-    parsedinput.append(line.split())
+    # use map to convert the list of strings to a list of ints
+    newline = map(int, line.split())
+    parsedinput.append(newline)
 
 # print triangle because visual learning
-for line in parsedinput:
-    print line
+# don't because it's huge on this problem
+#for line in parsedinput:
+#    print line
 
-pathlist = [(0, 0)]
-pathpoint = pathlist[0]
-print "start value, point: %d (%d, %d)" % (getValue(parsedinput, pathpoint),
-                                           pathpoint[0], pathpoint[1])
-for i in range(0, len(parsedinput)-3):
-    nextpathpoint = nextPathPoint(parsedinput, pathpoint)
-    print "next value, point: %d (%d, %d)" % (
-        getValue(parsedinput, nextpathpoint), nextpathpoint[0],
-        nextpathpoint[1])
-    pathlist.append(nextpathpoint)
-    pathpoint = nextpathpoint
-# Manually get the last two point
-choices = getNextPoints(parsedinput, pathlist[-1:][0])
-maxvalue = 0
-maxpoint = (0, 0)
-for choice in choices:
-    if getValue(parsedinput, choice) > maxvalue:
-        maxvalue = getValue(parsedinput, choice)
-        maxpoint = choice
-print "next value, point: %d (%d, %d)" % (maxvalue, maxpoint[0], maxpoint[1])
-pathlist.append(maxpoint)
-
-choices = getNextPoints(parsedinput, pathlist[-1:][0])
-maxvalue = 0
-maxpoint = (0, 0)
-for choice in choices:
-    if getValue(parsedinput, choice) > maxvalue:
-        maxvalue = getValue(parsedinput, choice)
-        maxpoint = choice
-print "next value, point: %d (%d, %d)" % (maxvalue, maxpoint[0], maxpoint[1])
-pathlist.append(maxpoint)
-
+pathlist = [[0, 0]]
+for x in range(len(parsedinput)-1):
+    pathlist.append(makechoice(parsedinput, pathlist[-1:][0]))
 
 totalsum = 0
-for point in pathlist:
-    totalsum = totalsum + getValue(parsedinput, point)
-print "totalsum: %d" % totalsum
+for coord in pathlist:
+    totalsum += getValue(parsedinput, coord)
+print 'totalsum:', totalsum
